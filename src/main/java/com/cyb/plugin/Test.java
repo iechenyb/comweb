10654da069e88b0e2453ca7b4a8cfb0e81fcccee
iechenyb <zzuchenyb@sina.com>
2017-03-14 13:53:53 +0800

    阿迪

---
+++ src/main/java/com/cyb/plugin/Test.java
@@ -0,0 +1,10 @@
+package com.cyb.plugin;
+
+import com.cyb.helloworld.impl.HelloImpl;
+
+public class Test {
+public static void main(String[] args) {
+	 HelloImpl hello = new HelloImpl(" hi ,iechenyb!");
+	 hello.sayHello();
+}
+}
---
+++ src/main/resources/sql/sys_user_type.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sys_user_type` VALUES (1, 'superadmin');
+INSERT INTO `sys_user_type` VALUES (2, 'admin');
+INSERT INTO `sys_user_type` VALUES (3, 'comuser');
